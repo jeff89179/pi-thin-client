f47b3fa13d32e716c75c524560f737e5976eb62f
Jeff <jeff89179@gmail.com>
2019-09-13 17:02:27 -0500

    Create rdesktop-connection.sh

---
+++ rdesktop-connection.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+### Fill in your username, password and host, don't include brackets ###
+### Not really secure to include the login in plain text, but this is not meant to connect to anything crucial ###
+rdesktop -u [user] -p [password] -f [host]
